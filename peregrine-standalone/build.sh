1210e76a5149b69a8f61769aa7662d2cc7bfa5b1
Manoj <manojpandian@ebi.ac.uk>
2021-04-07 10:08:47 +0100

    Draw api optional

--- peregrine-standalone/build.sh
+++ peregrine-standalone/build.sh
@@ -3,5 +3,5 @@
 set -e
 
 #RUSTFLAGS="" wasm-pack build --target web 
-RUSTFLAGS="--cfg=console --cfg=blackbox" wasm-pack build --target web --dev
+RUSTFLAGS="--cfg=console" wasm-pack build --target web --dev
 python3 -m http.server 8000
--- peregrine-standalone/src/lib.rs
+++ peregrine-standalone/src/lib.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate serde_json;
 
+use std::sync::{ Arc, Mutex };
 use std::fmt::Debug;
 mod standalonedom;
 use wasm_bindgen::prelude::*;
@@ -13,29 +15,12 @@ use peregrine_message::PeregrineMessage;
 pub use url::Url;
 use crate::standalonedom::make_dom;
 use web_sys::console;
+use serde_json::{Value, Map};
 
 lazy_static! {
-    static ref DRAW_API: PeregrineDraw = {
-        /*
-        * Set some config options. There aren't many of these yet but there will probably be more. The idea is that
-        * things like the speed of changes and positions, colours, cache sizes, etc, can come from the surrounding app.
-        * This isn't about the data in the tracks itself but there's probably going to be a whole load of configurable
-        * oojimaflips associated with the browser in the end.
-        */
-        let mut config = PeregrineConfig::new();
-        config.set_f64("animate.fade.slow",500.);
-        config.set_f64("animate.fade.fast",100.);
-        
-        /*
-        * Here we call standalonedom.rs which sorts out finding an element and setting it up for the genome browser to
-        * use. See that file for details.
-        */
-        let dom = js_throw(make_dom());
-
-        let mut draw_api: PeregrineDraw = js_throw(PeregrineDraw::new(config, dom));
-        draw_api
-    };
+    static ref DRAW_API: Arc<Mutex<Option<PeregrineDraw>>> = Arc::new(Mutex::new(None));
 }
+
 
 /*
  * This utility just catches handles serious errors in setting up this demonstration. It's not the main error-handling
@@ -51,32 +36,34 @@ pub fn js_throw<T,E: Debug>(e: Result<T,E>) -> T {
     }
 }
 
-
 /*
  * This function just does some daft stuff to kick the tyres for now. Don't worry too much about it. But it is quite
  * a good example of the sort of API calls you might make on receiving events from the browser chrome. The exact
  * name and arguments to this API are still up in the air, but you get the idea....
  */
 async fn test() -> anyhow::Result<()> {
-    DRAW_API.set_size(100.,100.);
-    DRAW_API.add_track(Track::new("gene-pc-fwd"));
-    DRAW_API.set_stick(&StickId::new("homo_sapiens_GCA_000001405_27:1"));
+    
+    let draw_api = DRAW_API.lock().unwrap().as_mut().unwrap();
+
+    draw_api.set_size(100.,100.);
+    draw_api.add_track(Track::new("gene-pc-fwd"));
+    draw_api.set_stick(&StickId::new("homo_sapiens_GCA_000001405_27:1"));
     let mut pos = 2500000.;
     let mut bp_per_screen = 1000000.;
-    for _ in 0..5 {
+    for _ in 0..20 {
         pos += 50000.;
-        let mut p = DRAW_API.set_x(pos);
+        let mut p = draw_api.set_x(pos);
         p.add_callback(move |v| {
             console::log_1(&format!("set_x({}) = {:?}",pos,v).into());
         });
-        let mut p = DRAW_API.set_bp_per_screen(bp_per_screen);
+        let mut p = draw_api.set_bp_per_screen(bp_per_screen);
         p.add_callback(move |v| {
             console::log_1(&format!("set_bp_per_screen({}) = {:?}",pos,v).into());
         });
         bp_per_screen *= 0.95;
         cdr_timer(1000.).await; // Wait one second
     }
-    let mut p = DRAW_API.set_stick(&StickId::new("invalid_stick"));
+    let mut p = draw_api.set_stick(&StickId::new("invalid_stick"));
     p.add_callback(move |v| {
         console::log_1(&format!("set_stick(*invalid*) = {:?}",v).into());
     });
@@ -84,26 +71,61 @@ async fn test() -> anyhow::Result<()> {
 }
 
 fn setup_genome_browser() -> Result<(),Message> {
+    /*
+     * Set some config options. There aren't many of these yet but there will probably be more. The idea is that
+     * things like the speed of changes and positions, colours, cache sizes, etc, can come from the surrounding app.
+     * This isn't about the data in the tracks itself but there's probably going to be a whole load of configurable
+     * oojimaflips associated with the browser in the end.
+     */
+    let mut config = PeregrineConfig::new();
+    config.set_f64("animate.fade.slow",500.);
+    config.set_f64("animate.fade.fast",100.);
+    /*
+     * Here we call standalonedom.rs which sorts out finding an element and setting it up for the genome browser to
+     * use. See that file for details.
+     */
+    let dom = make_dom()?;
+    /*
+     * Create a genome browser object.
+     */
+    let mut draw_api = DRAW_API.lock().unwrap();
+    *draw_api = Some(js_throw(PeregrineDraw::new(config,dom)));
     
-    DRAW_API.set_message_reporter(|message| {
+    let draw_api = DRAW_API.lock().unwrap().as_mut().unwrap();
+
+    /* 
+     * Configure the message reporter. This gets notifications of errors, etc. Note that this may be async to the
+     * request which caused it (eg it may be after a fair few AJAX calls that we finally realise some data we supplied
+     * was dodgy). Shortly there should be a mechanism to tie the message and originating request together.
+     */
+    draw_api.set_message_reporter(|message| {
         if !message.knock_on() && message.degraded_experience() {
-            console::error_1(&format!("{}",message).into());
+            let mut map = Map::new();
+            map.insert(String::from("type"), "error".into());
+            map.insert(String::from("message"), format!("{}",message).into());
+            map.insert(String::from("code"), 43.into());
+            let value = Value::Object(map);
+
+            let map: Map<String, Value> = serde_json::from_value(value).unwrap();
+            let js_error_value = js_throw(JsValue::from_serde(&map));
+ 
+            console::error_1(&js_error_value);
         }
     });
     /*
      * In general integrations probably don't want to set up blackbox, but I do here. It's a useful debug and
      * performance-tweaking tool. Just don't call this if you don't care.
      */
-    js_throw(DRAW_API.setup_blackbox("http://localhost:4040/blackbox/data"));
+    js_throw(draw_api.setup_blackbox("http://localhost:4040/blackbox/data"));
     /*
      * Ok, we're ready to go. Bootstrapping causes the genome browser to go to the backend and configure itself.
      */
     let url = "http://localhost:3333/api/data";
-    DRAW_API.bootstrap(Channel::new(&ChannelLocation::HttpChannel(js_throw(Url::parse(url)))));
+    draw_api.bootstrap(Channel::new(&ChannelLocation::HttpChannel(js_throw(Url::parse(url)))));
     /*
      * For now just start an async process to do some daft stuff to kick the tyres.
      */
-    let commander = DRAW_API.commander();
+    let commander = draw_api.commander();
     commander.add("test",100,None,None,Box::pin(test()));
     Ok(())
 }
@@ -124,14 +146,4 @@ pub fn main() -> Result<(), JsValue> {
 #[wasm_bindgen]
 pub fn init_panic_hook() {
     console_error_panic_hook::set_once();
-}
-
-
-
-#[wasm_bindgen]
-pub fn do_action(payload: JsValue) {
-
-    DRAW_API.add_track(Track::new("gene-pc-fwd"));
-    console::log_1(&format!("payload {:?}", payload.as_string()).into());
-
 }
