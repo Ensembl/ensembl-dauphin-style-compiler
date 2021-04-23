588f8962f33dfac1fad65d81143e138f028f2b81
Dan Sheppard <dan@ebi.ac.uk>
2021-04-23 17:37:54 +0100

    Track hash.
    Hash of all states of current track so output can be hashed.

--- peregrine-draw/src/switch/switch.rs
+++ peregrine-draw/src/switch/switch.rs
@@ -1,3 +1,4 @@
+use std::{collections::hash_map::DefaultHasher, hash::{ Hash, Hasher }};
 use std::sync::{ Arc, Mutex };
 use std::collections::HashMap;
 
@@ -37,35 +38,89 @@ impl Switch {
             kid.get_tracks(tracks);
         }
     }
+
+    fn set_hash<H: Hasher>(&self, hasher: &mut H) {
+        if !self.set { return; }
+        let mut kids : Vec<String> = self.kids.keys().cloned().collect();
+        kids.len().hash(hasher);
+        kids.sort();
+        for name in kids {
+            let kid = self.kids.get(&name).unwrap();
+            if kid.set {
+                name.hash(hasher);
+                kid.set_hash(hasher);
+            }
+        }
+    }
+}
+
+struct SwitchesData {
+    root: Switch,
+    track_paths: HashMap<String,Vec<Vec<String>>>,
+    track_hash_cache: HashMap<String,u64>
+}
+
+impl SwitchesData {
+    fn compute_track_hash(&mut self, track: &str) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        if let Some(paths) =  self.track_paths.get(track) {
+            let mut paths = paths.clone();
+            paths.sort();
+            for path in paths {
+                path.hash(&mut hasher);
+                self.root.get_target(&path.iter().map(|x| x.as_str()).collect::<Vec<_>>()).set_hash(&mut hasher);
+            }
+        }
+        hasher.finish()
+    }
 }
 
 #[derive(Clone)]
-pub struct Switches {
-    root: Arc<Mutex<Switch>>
-}
+pub struct Switches(Arc<Mutex<SwitchesData>>);
 
 impl Switches {
     pub fn new() -> Switches {
-        Switches {
-            root: Arc::new(Mutex::new(Switch::new()))
-        }
+        let out = Switches(Arc::new(Mutex::new(SwitchesData {
+            root: Switch::new(),
+            track_paths: HashMap::new(),
+            track_hash_cache: HashMap::new()
+        })));
+        out.set_switch(&[]);
+        out
     }
 
     pub fn set_switch(&self, path: &[&str]) {
-        self.root.lock().unwrap().get_target(path).set = true;
+        let mut data = self.0.lock().unwrap();        
+        data.root.get_target(path).set = true;
+        data.track_hash_cache.clear();
     }
 
     pub fn clear_switch(&self, path: &[&str]) {
-        self.root.lock().unwrap().get_target(path).set = false;        
+        let mut data = self.0.lock().unwrap();
+        data.root.get_target(path).set = false;        
+        data.track_hash_cache.clear();
     }
 
     pub fn add_track(&self, path: &[&str], track: &str) {
-        self.root.lock().unwrap().get_target(path).tracks.push(track.to_string());
+        let mut data = self.0.lock().unwrap();
+        data.root.get_target(path).tracks.push(track.to_string());
+        let paths = data.track_paths.entry(track.to_string()).or_insert_with(||vec![]);
+        paths.push(path.iter().map(|x| x.to_string()).collect());
+        data.track_hash_cache.clear();
     }
 
     pub fn get_tracks(&self) -> Vec<String> {
         let mut tracks = vec![];
-        self.root.lock().unwrap().get_tracks(&mut tracks);
+        self.0.lock().unwrap().root.get_tracks(&mut tracks);
         tracks
+    }
+
+    fn get_track_hash(&self, track: &str) -> u64 {
+        let mut data = self.0.lock().unwrap();
+        if !data.track_hash_cache.contains_key(track) {
+            let hash = data.compute_track_hash(track);
+            data.track_hash_cache.insert(track.to_string(),hash);
+        }
+        *data.track_hash_cache.get(track).unwrap()
     }
 }
