def14135761a5410cc542a4881875461a963ae3c
Dan Sheppard <dan@ebi.ac.uk>
2021-06-01 22:14:40 +0100

    You see a css property. Pick up css property. Throw css property at touch-event goblin.

--- peregrine-draw/src/input/low/mouseinput.rs
+++ peregrine-draw/src/input/low/mouseinput.rs
@@ -1,7 +1,8 @@
 use std::sync::Arc;
 use crate::{run::PgPeregrineConfig };
-use web_sys::{ MouseEvent, Event, WheelEvent };
+use web_sys::{ PointerEvent, Event, WheelEvent };
 use crate::util::{ Message };
+use crate::util::error::confused_browser;
 use super::{event::{ EventSystem }, lowlevel::LowLevelState };
 use crate::input::low::pointer::pointer::{ Pointer, PointerEventKind, PointerConfig };
 
@@ -26,16 +27,15 @@ impl MouseEventHandler {
         self.pointer.process_event(&self.config,&self.lowlevel,&self.position,&PointerEventKind::Up);
     }
 
-    fn mouse_event(&mut self, kind: &PointerEventKind, event: &MouseEvent) {
+    fn mouse_event(&mut self, kind: &PointerEventKind, event: &PointerEvent) {
         let rect = self.lowlevel.dom().canvas_frame().get_bounding_client_rect();
         let x = (event.client_x() as f64) - rect.left();
         let y = (event.client_y() as f64) - rect.top();
+        let id = event.pointer_id();
         self.position = (x,y);
         self.pointer.process_event(&self.config,&self.lowlevel,&self.position,kind);
-        match kind {
-            PointerEventKind::Move => {},
-            _ => { event.stop_propagation(); event.prevent_default(); }
-        }
+        event.stop_propagation();
+        event.prevent_default();
     }
 
     fn wheel_amount(&self, event: &WheelEvent) -> f64 {
@@ -58,22 +58,22 @@ impl MouseEventHandler {
 pub(super) fn mouse_events(config: &PgPeregrineConfig, state: &LowLevelState) -> Result<EventSystem<MouseEventHandler>,Message> {
     let mouse_config = Arc::new(PointerConfig::new(config)?);
     let dom = state.dom();
-    let body = dom.body();
     let canvas = dom.canvas();
     let mut events = EventSystem::new(MouseEventHandler::new(mouse_config,state));
-    events.add(canvas,"pointerdown", |handler,event| {
+    confused_browser(canvas.style().set_property("touch-action","none"))?;
+    events.add(canvas,"pointerdown", |handler,event: &PointerEvent| {
         handler.mouse_event(&PointerEventKind::Down,event)
     })?;
-    events.add(canvas,"pointerup", |handler,event| {
+    events.add(canvas,"pointerup", |handler,event: &PointerEvent| {
         handler.mouse_event(&PointerEventKind::Up,event)
     })?;
-    events.add(body,"pointermove", |handler,event| {
+    events.add(canvas,"pointermove", |handler,event: &PointerEvent| {
         handler.mouse_event(&PointerEventKind::Move,event)
     })?;
     events.add(canvas,"wheel", |handler,event| {
         handler.wheel_event(event)
     })?;
-    events.add(body,"pointerleave",|handler,event| {
+    events.add(canvas,"pointerleave",|handler,event| {
         handler.abandon(&event)
     })?;
     events.add(dom.canvas_frame(),"scroll",|_,_: &Event| {
