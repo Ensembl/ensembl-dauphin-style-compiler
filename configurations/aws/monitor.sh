d6d49f4121219024ffabfd1ada5817384b33808d
Dan Sheppard <dan@ebi.ac.uk>
2021-08-27 14:28:23 +0100

    Fix AWS moitoring script.

--- configurations/aws/monitor.sh
+++ configurations/aws/monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-watch 'tail -6 ~/gb/logs/syslog; echo; du -sh /cache 2>/dev/null ; echo ; docker ps --format "{{.ID}}   {{.Status}}   {{.Image}}"; echo; uptime"'
+watch 'tail -6 ~/gb/logs/syslog; echo; du -sh /cache 2>/dev/null ; echo ; docker ps --format "{{.ID}}   {{.Status}}   {{.Image}}"; echo; uptime'
