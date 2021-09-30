910bfdfe397ffc1e882627563018b98c143f9e44
Dan Sheppard <dan@ebi.ac.uk>
2021-09-30 14:22:53 +0100

    Automatically monitor begs file changes.

--- backend-server/app/command/begs.py
+++ backend-server/app/command/begs.py
@@ -1,5 +1,5 @@
 from core.config import BEGS_FILES, BEGS_CONFIG
-from typing import Any;
+from typing import Any, Optional;
 import logging
 import toml
 import time
@@ -9,7 +9,28 @@ from os import stat
 
 class BegsFilesMonitor(object):
     def __init__(self):
+        self._paths = {}
+        self._mtimes = {}
         pass
+
+    def add(self, name: str, path: str):
+        self._paths[name] = path
+        self.check(name)
+
+    def check(self, name: str) -> bool:
+        path = self._paths[name]
+        if path == None:
+            return False
+        old_mtime = self._mtimes.get(path,0)
+        new_mtime = os.stat(path).st_mtime
+        if old_mtime != new_mtime:
+            self._mtimes[path] = new_mtime
+            return True
+        else:
+            return False
+
+    def path(self, name: str) -> Optional[str]:
+        return self._paths.get(name)
 
 class BegsFiles(object):
     def __init__(self):
@@ -28,17 +49,18 @@ class BegsFiles(object):
         self.bundle_contents = {}
         self.program_map = {}
         self.program = {}
+        self._monitor = BegsFilesMonitor()
         for (name_of_bundle,mapping) in toml_file["begs"].items():
             program_path = os.path.join(
                 BEGS_FILES,
                 "{}.begs".format(name_of_bundle)
             )
+            self._monitor.add(name_of_bundle,program_path)
             self.program[name_of_bundle] = self.load_program(program_path)
             self.bundle_contents[name_of_bundle] = {}
             for (name_in_bundle,name_in_channel) in mapping.items():
                 self.program_map[name_in_channel] = (name_of_bundle,name_in_bundle)
                 self.bundle_contents[name_of_bundle][name_in_channel] = name_in_bundle
-        self._monitor = BegsFilesMonitor()
 
     def find_bundle(self, name: str) -> str:
         v = self.program_map[name]
@@ -54,6 +76,9 @@ class BegsFiles(object):
             return cbor2.loads(f.read())
 
     def add_bundle(self, bundle_name: str) -> Any:
+        if self._monitor.check(bundle_name):
+            logging.warn("Bundle '{0}' changed. Reloading".format(bundle_name))
+            self.program[bundle_name] = self.load_program(self._monitor.path(bundle_name))
         return Bundle(self,bundle_name).serialize(self.program[bundle_name])
 
 class Bundle(object):
