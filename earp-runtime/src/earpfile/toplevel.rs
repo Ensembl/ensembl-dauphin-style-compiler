45742d911d73ca8f6efe9068d931ececd3119e41
Dan Sheppard <dan@ebi.ac.uk>
2022-01-30 02:52:12 +0000

    More robust parsing.

--- earp-runtime/src/earpfile/toplevel.rs
+++ earp-runtime/src/earpfile/toplevel.rs
@@ -34,6 +34,29 @@ fn extract_varieties(mut input: usize) -> Vec<usize> {
     out
 }
 
+fn cbor_iter<F,G>(decoder: &mut Decoder, mut cb: F, start: G) -> Result<(),minicbor::decode::Error> 
+        where F: FnMut(&mut Decoder) -> Result<bool,minicbor::decode::Error>,
+              G: FnOnce(&mut Decoder) -> Result<Option<u64>,minicbor::decode::Error> {
+    if let Some(len) = start(decoder)? {
+        for _ in 0..len {
+            cb(decoder)?;
+        }
+    } else {
+        while cb(decoder)? {}
+    }
+    Ok(())
+}
+
+fn cbor_array<F>(decoder: &mut Decoder, cb: F) -> Result<(),minicbor::decode::Error> 
+        where F: FnMut(&mut Decoder) -> Result<bool,minicbor::decode::Error> {
+    cbor_iter(decoder,cb,|decoder| decoder.array())
+}
+
+fn cbor_map<F>(decoder: &mut Decoder, cb: F) -> Result<(),minicbor::decode::Error> 
+        where F: FnMut(&mut Decoder) -> Result<bool,minicbor::decode::Error> {
+    cbor_iter(decoder,cb,|decoder| decoder.map())
+}
+
 impl<'b> Decode<'b> for TopLevel {
     fn decode(decoder: &mut Decoder<'b>) -> Result<Self, minicbor::decode::Error> {
         let mut magic_got = None;
@@ -48,40 +71,56 @@ impl<'b> Decode<'b> for TopLevel {
                     magic_got = Some(decoder.str()?.to_string());
                 },
                 Ok("S") => {
-                    decoder.array()?;
-                    while let Ok(name) = decoder.str() {
-                        let version = decoder.u64()?;
-                        let offset = decoder.u64()?;
-                        sets.push((name.to_string(),version,offset));
-                    }
+                    cbor_array(decoder,|decoder| {
+                        Ok(if let Ok(name) = decoder.str() {
+                            let version = decoder.u64()?;
+                            let offset = decoder.u64()?;
+                            sets.push((name.to_string(),version,offset));
+                            true
+                        } else {
+                            false
+                        })
+                    })?;
                 },
                 Ok("E") => {
-                    decoder.map()?;
-                    while let Ok(key) = decoder.str() {
-                        let value = decoder.i64()?;
-                        entry_points.insert(key.to_string(),value);
-                    }
+                    cbor_map(decoder,|decoder| {
+                        Ok(if let Ok(key) = decoder.str() {
+                            let value = decoder.i64()?;
+                            entry_points.insert(key.to_string(),value);
+                            true
+                        } else {
+                            false
+                        })
+                    })?;
                 },
                 Ok("I") => {
-                    decoder.array()?;
-                    while let Ok(opcode) = decoder.u64() {
-                        let mut operands = vec![];
-                        for variety in extract_varieties(decoder.u64()? as usize) {
-                            operands.push(Operand::decode(variety,decoder)?);
-                        }
-                        instructions.push((opcode,operands));
-                    }
+                    cbor_array(decoder,|decoder| {
+                        Ok(if let Ok(opcode) = decoder.u64() {
+                            let mut operands = vec![];
+                            for variety in extract_varieties(decoder.u64()? as usize) {
+                                operands.push(Operand::decode(variety,decoder)?);
+                            }
+                            instructions.push((opcode,operands));
+                            true
+                        } else {
+                            false
+                        })
+                    })?;
                 },
                 Ok("A") => {
-                    decoder.map()?;
-                    while let Ok(key) = decoder.str() {
-                        let value = if decoder.probe().str().is_ok() {
-                            AssetData::String(decoder.str()?.to_string())
+                    cbor_map(decoder,|decoder| {
+                        Ok(if let Ok(key) = decoder.str() {
+                            let value = if decoder.probe().str().is_ok() {
+                                AssetData::String(decoder.str()?.to_string())
+                            } else {
+                                AssetData::Bytes(decoder.bytes()?.to_vec())
+                            };
+                            assets.insert(key.to_string(),value);
+                            true
                         } else {
-                            AssetData::Bytes(decoder.bytes()?.to_vec())
-                        };
-                        assets.insert(key.to_string(),value);
-                    }
+                            false
+                        })
+                    })?;
                 },
                 Ok(_) => {
                     decoder.skip()?;
