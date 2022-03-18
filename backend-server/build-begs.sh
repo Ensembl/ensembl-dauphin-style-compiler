d8f55cc6f3a9ae876e56ceca4a686c9a5db25c13
Dan Sheppard <dan@ebi.ac.uk>
2022-03-18 14:01:52 +0000

    start of transcript track with new allotments.

--- backend-server/build-begs.sh
+++ backend-server/build-begs.sh
@@ -8,6 +8,6 @@ SRC="$DIR/egs-data/egs"
 DST="$DIR/egs-data/begs"
 
 #dauphin -c $SRC/variant.egs -c $SRC/contig.egs -c $SRC/zoomed-seq.egs -c $SRC/gene.egs -c $SRC/transcript.egs -c $SRC/gene-overview.egs -c $SRC/gc.egs -c $SRC/zoomed-transcript-6.egs -c $SRC/ruler.egs -c $SRC/framing.egs -o $DST/render8.begs -L peregrine -O2 
-dauphin -c $SRC/gene8.egs -c $SRC/gc8.egs -c $SRC/variant8.egs -c $SRC/contig8.egs -c $SRC/ruler8.egs -c $SRC/framing8.egs -o $DST/render8.begs -L peregrine -O2 
+dauphin -c $SRC/gene8.egs -c $SRC/transcript8.egs -c $SRC/gc8.egs -c $SRC/variant8.egs -c $SRC/contig8.egs -c $SRC/ruler8.egs -c $SRC/framing8.egs -o $DST/render8.begs -L peregrine -O2 
 dauphin -c $SRC/startup8.egs -c $SRC/lookup.egs -c $SRC/jump.egs -o $DST/stick8.begs -L peregrine -O2
 dauphin -c $SRC/boot.egs -o $DST/boot8.begs -L peregrine -O2
