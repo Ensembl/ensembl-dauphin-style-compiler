1060177905f9a8a659f5d6fd22a9082e58237b92
Mehrnaz Charkhchi <mehrnaz.charkhchi@gmail.com>
2022-04-27 15:38:43 +0100

    Refactor based on PEP8 and add docstrings.

--- backend-server/app/model/chromosome.py
+++ backend-server/app/model/chromosome.py
@@ -2,13 +2,31 @@ import logging
 import os.path
 from model.datalocator import AccessItem
 
+
 def chrless(x):
+    """
+
+    Args:
+        x ():
+
+    Returns:
+        list
+    """
     if x.startswith("chr"):
         return x[3:]
     else:
         return x
 
+
 class Chromosome(object):
+    """
+
+    Args:
+        name ():
+        size ():
+        seq_hash ():
+        species ():
+    """
     def __init__(self, name, size, seq_hash, species):
         self.name = name
         self.size = size
@@ -17,13 +35,29 @@ class Chromosome(object):
         self.seq_hash = seq_hash
         self.genome_id = species.genome_id
         self.stick_name = "{0}:{1}".format(
-            species.wire_id,self.name
+            species.wire_id, self.name
         )
         self.genome_path = species.genome_id
         self.wire_id = chrless(self.name)
 
-    def item_path(self,variety):
-        return AccessItem(variety,self.genome_id,self.name)
+    def item_path(self, variety):
+        """
 
-    def item_seq_path(self,variety):
-        return AccessItem(variety,self.genome_id,self.seq_hash)
+        Args:
+            variety ():
+
+        Returns:
+            AccessItem:
+        """
+        return AccessItem(variety, self.genome_id, self.name)
+
+    def item_seq_path(self, variety):
+        """
+
+        Args:
+            variety ():
+
+        Returns:
+            AccessItem:
+        """
+        return AccessItem(variety, self.genome_id, self.seq_hash)
