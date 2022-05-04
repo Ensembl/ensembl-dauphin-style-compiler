d8f61aaddd6c00937f7459c001045a0484454e09
Dan Sheppard <dan@ebi.ac.uk>
2022-05-04 18:04:42 +0100

    Document bigbed.py.

--- backend-server/app/model/bigbed.py
+++ backend-server/app/model/bigbed.py
@@ -6,8 +6,8 @@ from core.exceptions import RequestException
 
 """
 Attributes:
-    _bigbeds (dict)
-    _bigwigs (dict)
+    _bigbeds (dict) A cache of open bigbed files, for efficiency
+    _bigwigs (dict) A cache of open bigwig files, for efficiency
 """
 _bigbeds = {}
 _bigwigs = {}
@@ -15,15 +15,19 @@ _bigwigs = {}
 
 def _get_bigbed_data(path, chrom, start, end):
     """
+    Retrieve data lines from bigbed file.
+
+    This low-level, file-internal function is only used by get_bigbed further down this
+    file.
 
     Args:
-        path ():
-        chrom ():
-        start (int):
-        end (int):
+        path (str): file path of bigbed file
+        chrom (str): chromosome name as used in bigbed file
+        start (int): start co-ordinate of range of interest
+        end (int): end co-ordinate of range of interest
 
     Returns:
-        list
+        List of 3-tuples each being: start coordinate; end coordinate; rest of string
 
     """
     end = min(end, chrom.size)
@@ -37,19 +41,28 @@ def _get_bigbed_data(path, chrom, start, end):
     return out
 
 
-def get_bigwig_stats_data(path, chrom, start, end, consolidation="mean", nBins=500):
+def _get_bigwig_stats_data(path, chrom, start, end, consolidation="mean", nBins=500):
     """
+    Retrieve binned data from bigwig file. Binned data is data where each data-point
+    represents a summary of more than one data-point in the file, consolidtaed with
+    the given function, and so is useful at more zoomed-out levels.
+
+    This low-level, file-internal function is only used by get_bigwig_stats further down
+    this file.
 
     Args:
-        path ():
-        chrom ():
-        start (int):
-        end (int):
-        consolidation (str):
-        nBins ():
+        path (str): path to bigwig file
+        chrom (str): chromosome in bigwig file
+        start (int): start coordinate of region of interest
+        end (int): end coordinate of region of interest
+        consolidation (str): method to use to reduce multiple values to single datapoint
+        nBins (int): number of bins (ie datapoints) to produce
 
     Returns:
-        tuple(list, int, int)
+        tuple(list, int, int): first value is the actual datapoints, other two are the
+        actual start and end used, which will have expanded a little over the start and
+        end given. This is needed to assume that there are no edge-effects from the
+        binning opertaion.
     """
     # angel's share: extra at start end end to allow seamless overlap
     angel_share = int((end - start) / nBins) + 1
@@ -69,17 +82,25 @@ def get_bigwig_stats_data(path, chrom, start, end, consolidation="mean", nBins=5
     return out, start, end
 
 
-def get_bigwig_data(path, chrom, start, end):
+def _get_bigwig_data(path, chrom, start, end):
     """
+    Retireve raw datapoints from a bigwig file. As every datapoint in the file within
+    the region is returned, this is only useful when very zoomed in.
+
+    This low-level, file-internal function is only used by get_bigwig further down this
+    file.
 
     Args:
-        path ():
-        chrom ():
-        start (int):
-        end (int):
+        path (str): path to bigwig file
+        chrom (str): chromosome name
+        start (int): start coordinate of region of interest
+        end (int): end coordinate of region of interest
 
     Returns:
-        tuple(list, int, int)
+        tuple(list, int, int): first value is the actual datapoints, other two are the
+        actual start and end used, which will have expanded a little over the start and
+        end given. This is needed to assume that there are no edge-effects from the
+        binning opertaion.
     """
     end = min(end, chrom.size)
     if end < start:
@@ -96,14 +117,17 @@ def get_bigwig_data(path, chrom, start, end):
 
 def get_bigbed(data_accessor: DataAccessor, item: AccessItem, start: int, end: int):
     """
+    Retrieve bigbed data using am AccessItem, which is an abstraction over a source of
+    some data and might map to a local file or a url, etc.
 
     Args:
-        data_accessor ():
-        item ():
-        start (int):
-        end (int):
+        data_accessor (DataAccessor): singleton business object containing location-resolution code
+        item (AccessItem): some data source in bigbed format
+        start (int): start coordinate of region of interest
+        end (int): end coordinate of region of interest
 
     Returns:
+        List of 3-tuples each being: start coordinate; end coordinate; rest of string
 
     """
     accessor = data_accessor.resolver.get(item)
@@ -120,24 +144,29 @@ def get_bigbed(data_accessor: DataAccessor, item: AccessItem, start: int, end: i
 
 def get_bigwig(data_accessor: DataAccessor, item: AccessItem, start: int, end: int):
     """
+    Retireve raw datapoints from a bigwig file. As every datapoint in the file within
+    the region is returned, this is only useful when very zoomed in.
 
     Args:
-        data_accessor ():
-        item ():
-        start (int):
-        end (int):
+        data_accessor (DataAccessor): singleton business object containing location-resolution code
+        item (AccessItem): some data source in bigbed format
+        start (int): start coordinate of region of interest
+        end (int): end coordinate of region of interest
 
     Returns:
-
+        tuple(list, int, int): first value is the actual datapoints, other two are the
+        actual start and end used, which will have expanded a little over the start and
+        end given. This is needed to assume that there are no edge-effects from the
+        binning opertaion.
     """
     accessor = data_accessor.resolver.get(item)
     chromosome = data_accessor.data_model.stick(data_accessor, item.stick())
     if accessor is None:
         return [], start, end
     if accessor.file is not None:
-        return get_bigwig_data(accessor.file, chromosome, start, end)
+        return _get_bigwig_data(accessor.file, chromosome, start, end)
     elif accessor.url is not None:
-        return get_bigwig_data(accessor.url, chromosome, start, end)
+        return _get_bigwig_data(accessor.url, chromosome, start, end)
     else:
         raise RequestException("cannot use accessor to get data")
 
@@ -145,25 +174,34 @@ def get_bigwig(data_accessor: DataAccessor, item: AccessItem, start: int, end: i
 def get_bigwig_stats(data_accessor: DataAccessor, item: AccessItem, start: int, end: int, consolidation: str = "mean",
                      nBins: int = 1000):
     """
+    Retrieve binned data from bigwig file. Binned data is data where each data-point
+    represents a summary of more than one data-point in the file, consolidtaed with
+    the given function, and so is useful at more zoomed-out levels.
+
+    This low-level, file-internal function is only used by get_bigwig_stats further down
+    this file.
 
     Args:
-        data_accessor ():
-        item ():
-        start (int):
-        end (int):
-        consolidation ():
-        nBins ():
+        data_accessor (DataAccessor): singleton business object containing location-resolution code
+        item (AccessItem): some data source in bigbed format
+        start (int): start coordinate of region of interest
+        end (int): end coordinate of region of interest
+        consolidation (str): method to use to reduce multiple values to single datapoint
+        nBins (int): number of bins (ie datapoints) to produce
 
     Returns:
-
+        tuple(list, int, int): first value is the actual datapoints, other two are the
+        actual start and end used, which will have expanded a little over the start and
+        end given. This is needed to assume that there are no edge-effects from the
+        binning opertaion.
     """
     accessor = data_accessor.resolver.get(item)
     chromosome = data_accessor.data_model.stick(data_accessor, item.stick())
     if accessor is None:
         raise RequestException("Cannot resolve item")
     if accessor.file is not None:
-        return get_bigwig_stats_data(accessor.file, chromosome, start, end, consolidation=consolidation, nBins=nBins)
+        return _get_bigwig_stats_data(accessor.file, chromosome, start, end, consolidation=consolidation, nBins=nBins)
     elif accessor.url is not None:
-        return get_bigwig_stats_data(accessor.url, chromosome, start, end, consolidation=consolidation, nBins=nBins)
+        return _get_bigwig_stats_data(accessor.url, chromosome, start, end, consolidation=consolidation, nBins=nBins)
     else:
         raise RequestException("cannot use accessor to get data")
