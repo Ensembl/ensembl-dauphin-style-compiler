9133c2f9ef2a8c37419083025dc04270ceb90a40
Dan Sheppard <dan@ebi.ac.uk>
2022-05-05 11:38:45 +0100

    Document slider.

--- peregrine-data/src/train/switcher.rs
+++ peregrine-data/src/train/switcher.rs
@@ -1,20 +1,51 @@
-/* Switcher abstracts the train-change operations using type polymorphism. The categories
- * are:
+/* Consider a set of objects T0, T1, T2, ..., indexed by specifications x0, x1, x2.
+ * After startup, exactly one Tk is "current" at any moment in time. External calls set
+ * the desired current object from time to time by speficfying an xk. However, creating
+ * an object Tk from xk and making such an object current is not an instantaneous 
+ * operation. In this case the current object retains its current value until the new
+ * object is ready. During this time, a new x, say xm, may be specified. In this case the 
+ * work currently in progess on Tk is abandoned and the new Tm is prepared. In addition,
+ * even when an object is ready to be current, the transition itself may not be
+ * instantaneous, however at this point it must be carried on to completion before the
+ * new current is esrablished. It could also be that this process overwhelms execution
+ * with rapidly changing requests, which needs to be guarded against. These tasks are the
+ * job of Switcher. Siwtcher only performs operations on a regular clock-tick to ping().
+ * This helps keep the many concurrent processes under some control.
  * 
- * current: the currently displayed train (or fading out, if fading)
- * future: the fading in train, if fading
- * wanted: the train we are currently collecting data for
- * target: the train extent which would be ideal for the given coordinates
+ * Some Tm, known as "milestones", may be temporarily "good enough" replacements for a
+ * set of other {Ta,Tb,...}. When very busy, the switcher can decide to temporarily render
+ * Tm instead of Ta,Tb,... in order to reduce workload. When Tm is being made current,
+ * requests for Ta, Tb, etc can be regarded as no-ops until the workload decreases. If
+ * the external code sets the variable "sketchy" they are aware of a great deal of
+ * activity, and milestones are sufficient until this ends.
+ *
+ * The following traits must be implemented:
+ * SwitcherManager -- a means of creating objects, Tk, from specitications, xk.
+ * SwitcherExtent -- a specification, xk.
+ * SwitcherObject -- an object Tk.
  * 
+ * Throughout the code in this file, the following terminology is used:
+ *
+ * current: the current object (or transitioning, the object transitioning out).
+ * future: the object transitioniing in, if any.
+ * wanted: the object currently being constructed, if any.
+ * target: the train extent requested by the user.
  * 
  */
 
+ /* The SwitcherManager provides the instance-specific details of a particular
+  * implementation. It is also accessible by Switcher's manager() and manager_mut()
+  * so can include any instance-specific functionality.
+  */
 pub(super) trait SwitcherManager {
     type Type: SwitcherObject;
     type Extent: SwitcherExtent;
     type Error;
 
+    /* create an instance Tk from a spec xk */ 
     fn create(&mut self, extent: &Self::Extent) -> Result<Self::Type,Self::Error>;
+
+    /* callback to the manager when the Slider is busy, ie crating or transitioning. */
     fn busy(&self, yn: bool);
 }
 
@@ -22,7 +53,10 @@ pub(super) trait SwitcherExtent : Eq {
     type Type: SwitcherObject;
     type Extent: SwitcherExtent;
 
+    /* convert given specification to its corresponding milestone */
     fn to_milestone(&self) -> Self::Extent;
+
+    /* test whether self serves as a milestone for what */
     fn is_milestone_for(&self, what: &Self::Extent) -> bool;
 }
 
@@ -31,15 +65,33 @@ pub(super) trait SwitcherObject {
     type Type;
     type Speed;
 
-    /* queries */
+    /*** Queries made by Slider of a SwitcherObject ***/
+
+    /* Return spec, xk, corresponding to this Tk. */
     fn extent(&self) -> Self::Extent;
+
+    /* An object is "half-ready" if it is ready enough to be the _first_ current item.
+     * This is usually a lower bar than subsequent current items.
+     */
     fn half_ready(&self) -> bool;
+
+    /* An object is "ready" if it can be made current */
     fn ready(&self) -> bool;
+
+    /* An object which is broken must never become current due to a problem during build. */
     fn broken(&self) -> bool;
+
+    /* Transitions can occur at different speeds (speed type is implementation-specific).
+     * By comparing the ingoing and outgoing object (if any), a Speed is returned.
+     */
     fn speed(&self, source: Option<&Self::Type>) -> Self::Speed;
 
-    /* events */
+    /*** Events raised by Slider to manipulate a SwitcherObject ***/
+
+    /* Object is now to be the current object, transitioning at the given speed */
     fn live(&mut self, speed: &Self::Speed);
+
+    /* Object is no longer current and may be deallocated */
     fn dead(&mut self);
 }
 
@@ -68,7 +120,9 @@ impl<M:SwitcherManager<Extent=X,Type=T,Error=E>,
         }
     }
 
-    /* The quiescent train is the train which, barring this and any future changes will ultimately be displayed. */
+    /* The quiescent object is the object which, barring this and any future changes will
+     * ultimately become current.
+     */
     pub(super) fn quiescent(&self) -> Option<&T> {
         if let Some(wanted) = &self.wanted { return Some(wanted); }
         if let Some(future) = &self.future { return Some(future); }
@@ -76,37 +130,20 @@ impl<M:SwitcherManager<Extent=X,Type=T,Error=E>,
         None
     }
 
+    /* Call given callback on all objects currently displayed or being prepared. */
     pub(super) fn each_mut<F>(&mut self, cb: &F) where F: Fn(&mut T) {
         if let Some(wanted) = &mut self.wanted { cb(wanted); }
         if let Some(future) = &mut self.future { cb(future); }
         if let Some(current) = &mut self.current { cb(current); }
     }
 
-    /* something has happened which may cause maneouverability */
-    pub(super) fn ping(&mut self) -> Result<(),E> {
-        /* try wanted->future in case that is required for target->wanted */
-        self.try_wanted_to_future();
-        self.try_target_to_wanted()?;
-        /* Maybe wanted can fall straight through? */
-        self.try_wanted_to_future();
-        /* Are we busy? */
-        let now_busy = self.future.is_some() || self.wanted.is_some();
-        if now_busy != self.was_busy {
-            self.was_busy = now_busy;
-            self.manager.busy(now_busy);
-        }
-        Ok(())
-    }
-
-    pub(super) fn get_target(&self) -> Option<&X> { self.target.as_ref() }
-    pub(super) fn set_target(&mut self, extent: &X) -> Result<(),E> {
-        if self.target.is_none() || self.target.as_ref().unwrap() != extent {
-            self.target = Some(extent.clone());
-            self.ping()?;
-        }
-        Ok(())
-    }
-
+    /* Part of the ping process. A predicate which determines whether we should carry
+     * on building "wanted". We are fine if the quiescent value will match the target
+     * exactly or if wanted is a milestone for our target and there is currently something
+     * in wanted. (Once that has cleared from wanted, this will cause it to skip from the
+     * milestone to the actual target: milestones are only acceptable while there is
+     * something already filling "wanted").
+     */
     fn target_matches_wanted(&mut self) -> bool {
         /* No target so say match to avoid trying further */
         if self.target.is_none() { return true; }
@@ -122,31 +159,41 @@ impl<M:SwitcherManager<Extent=X,Type=T,Error=E>,
         false
     }
 
+    /* Part of the ping process. The "user" has set the target. If we are not happy
+     * with the current value of wanted (assessed by target_matches_wanted), then
+     * build a new wanted.
+     */
     fn try_target_to_wanted(&mut self) -> Result<(),E> {
-        /* We're heading somewhere already, is it the target? */
+        /* Exit if we are happy with the way things are */
         if self.target_matches_wanted() { return Ok(()); }
         let target = self.target.as_ref().unwrap();
-        /* Aim direct, or to milestone? */
+        /* Where do we ain: direct, or to milestone? We aim for milestones if
+         * there is already something in wanted which we are evicting (thrashing) or if
+         * the user has set "sketchy" to indicate there is a great deal of interaction
+         * going on.
+         */
         let extent = if self.wanted.is_some() || self.sketchy {
             target.to_milestone()
         } else {
             target.clone()
         };
-        /* drop old wanted and make milestone, if necessary */      
+        /* drop old wanted, if necessary */      
         if let Some(mut wanted) = self.wanted.take() { wanted.dead(); }
-        /* do it */
+        /* create a new one */
         self.wanted = Some(self.manager.create(&extent)?);
         Ok(())
     }
 
+    /* Part of the ping process. Is the object currently being constructed in wanted
+     * now in a ready-enough state that we can start the transition? Is so, do so.
+     */
     fn try_wanted_to_future(&mut self) {
-        /* given the general circumstances, are we good to go? */
+        /* Is wanted ready and is there space in "future"? */
         let desperate = self.future.is_none() && self.current.is_none();
         let good_enough = self.wanted.as_ref().map(|x| {
             let ready_enough = x.ready() || (desperate && x.half_ready());
             ready_enough && !x.broken()
         }).unwrap_or(false);
-        /* bail unless we can, and want to commit */
         if !good_enough || self.future.is_some() { return; }
         /* do it */
         let wanted = self.wanted.take().unwrap();
@@ -155,6 +202,40 @@ impl<M:SwitcherManager<Extent=X,Type=T,Error=E>,
         self.future.as_mut().unwrap().live(&speed);
     }
 
+    /* Call periodically on a clock "beat" to cause Switcher to act. See the
+     * comments on the individual try_* methods for a secription of the process.
+     */
+    pub(super) fn ping(&mut self) -> Result<(),E> {
+        /* try wanted->future in case that is required for target->wanted */
+        self.try_wanted_to_future();
+        self.try_target_to_wanted()?;
+        /* Maybe wanted can fall straight through? */
+        self.try_wanted_to_future();
+        /* Update busy in manager */
+        let now_busy = self.future.is_some() || self.wanted.is_some();
+        if now_busy != self.was_busy {
+            self.was_busy = now_busy;
+            self.manager.busy(now_busy);
+        }
+        Ok(())
+    }
+
+    /* Utility method for an external caller to see the current target (which they will
+     * have set with set_target()) */
+    pub(super) fn get_target(&self) -> Option<&X> { self.target.as_ref() }
+
+    /* Set a new current target */
+    pub(super) fn set_target(&mut self, extent: &X) -> Result<(),E> {
+        if self.target.is_none() || self.target.as_ref().unwrap() != extent {
+            self.target = Some(extent.clone());
+            self.ping()?;
+        }
+        Ok(())
+    }
+
+    /* *Called by the object_being made current to indicate that this process is now
+     * complete.
+     */
     pub(super) fn live_done(&mut self) -> Result<(),E> {
         /* retire current and make future current */
         if let Some(mut current) = self.current.take() {
@@ -165,11 +246,15 @@ impl<M:SwitcherManager<Extent=X,Type=T,Error=E>,
         self.ping()
     }
 
+    /* Set "sketchy" mode which is a teporary mode whereby milestones are preferred */
     pub(super) fn set_sketchy(&mut self, yn: bool) -> Result<(),E> {
         self.sketchy = yn;
         self.ping()
     }
 
+    /* Retrieve manager reference for implementation-specific calls */
     pub(super) fn manager(&self) -> &M { &self.manager }
+
+    /* Retrieve mutable manager reference  for implementation-specific calls */
     pub(super) fn manager_mut(&mut self) -> &mut M { &mut self.manager }
 }
