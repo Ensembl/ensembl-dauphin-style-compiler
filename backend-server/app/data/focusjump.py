00fe1eace1e6fd2c3720417a746e20d6e8eea67e
Dan Sheppard <dan@ebi.ac.uk>
2022-05-27 16:34:11 +0100

    Focus jump cache lacked version.

--- backend-server/app/data/focusjump.py
+++ backend-server/app/data/focusjump.py
@@ -6,6 +6,7 @@ from command.coremodel import DataAccessor
 from model.datalocator import AccessItem
 from ncd import NCDRead
 from util.string import split_all
+from model.version import Version
 
 PREFIX = "focus:"
 
@@ -15,17 +16,17 @@ class FocusJumpHandler:
         accessor = data_accessor.resolver.get(item)
         self._jump_ncd = NCDRead(accessor.ncd())
 
-    def get(self,data_accessor: DataAccessor,location: str):
+    def get(self,data_accessor: DataAccessor, location: str, version: Version):
         if location.startswith(PREFIX):
             for (species,chrom) in split_all(":",location[len(PREFIX):]):
                 key = PREFIX + species
-                cached = data_accessor.cache.get_jump(location)
+                cached = data_accessor.cache.get_jump(location,version)
                 if cached != None:
                     return cached
                 value = self._jump_ncd.get(location.encode("utf-8"))
                 if value != None:
                     parts = value.decode("utf-8").split("\t")
                     out = (species+":"+parts[0],int(float(parts[1])),int(float(parts[2])))
-                    data_accessor.cache.set_jump(location,*out)
+                    data_accessor.cache.set_jump(location,*out,version)
                     return out
         return None
