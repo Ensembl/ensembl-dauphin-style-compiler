a849ccb5336dd34739c8c1c6ea0bae9ae08d0b2c
Dan Sheppard <dan@ebi.ac.uk>
2022-06-24 12:26:52 +0100

    Use new source files for 0.4.0 where necessary.

--- backend-server/build-begs.sh
+++ backend-server/build-begs.sh
@@ -7,6 +7,6 @@ export PATH="$PATH:$DIR/../dauphin/target/release"
 SRC="$DIR/egs-data/egs"
 DST="$DIR/egs-data/begs"
 
-dauphin -c $SRC/zoomed-seq8.egs -c $SRC/gene-overview8.egs -c $SRC/gene8.egs -c $SRC/transcript8.egs -c $SRC/zoomed-transcript8.egs -c $SRC/gc8.egs -c $SRC/variant8.egs -c $SRC/contig8.egs -c $SRC/ruler8.egs -c $SRC/framing8.egs -o $DST/render9.begs -L peregrine -O2 
+dauphin -c $SRC/zoomed-seq8.egs -c $SRC/gene-overview8.egs -c $SRC/gene8.egs -c $SRC/transcript9.egs -c $SRC/zoomed-transcript9.egs -c $SRC/gc8.egs -c $SRC/variant8.egs -c $SRC/contig8.egs -c $SRC/ruler8.egs -c $SRC/framing8.egs -o $DST/render9.begs -L peregrine -O2 
 dauphin -c $SRC/startup8.egs -c $SRC/lookup.egs -c $SRC/jump.egs -o $DST/stick9.begs -L peregrine -O2
 dauphin -c $SRC/boot.egs -o $DST/boot9.begs -L peregrine -O2
