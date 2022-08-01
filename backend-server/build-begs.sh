c80a1ab4214739d7d4b95d82f38a058cc069ca63
Dan Sheppard <dan@ebi.ac.uk>
2022-08-01 04:24:05 +0100

    Move 0.4.3 onto API v12.

--- backend-server/build-begs.sh
+++ backend-server/build-begs.sh
@@ -7,6 +7,6 @@ export PATH="$PATH:$DIR/../dauphin/target/release"
 SRC="$DIR/egs-data/egs"
 DST="$DIR/egs-data/begs"
 
-dauphin -c $SRC/v11/zoomed-seq.egs -c $SRC/v11/gene-overview.egs -c $SRC/v11/gene.egs -c $SRC/v11/transcript.egs -c $SRC/v11/zoomed-transcript.egs -c $SRC/v11/gc.egs -c $SRC/v11/variant.egs -c $SRC/v11/contig.egs -c $SRC/v11/ruler.egs -c $SRC/v11/framing.egs -c $SRC/v11/focus-transcript.egs -c $SRC/v11/focus-zoomed-transcript.egs -o $DST/render11.begs -L peregrine -O2 
-dauphin -c $SRC/v11/startup.egs -c $SRC/v11/lookup.egs -c $SRC/v11/jump.egs -o $DST/stick11.begs -L peregrine -O2
-dauphin -c $SRC/v11/boot.egs -o $DST/boot11.begs -L peregrine -O2
+dauphin -c $SRC/v12/zoomed-seq.egs -c $SRC/v12/gene-overview.egs -c $SRC/v12/gene.egs -c $SRC/v12/transcript.egs -c $SRC/v12/zoomed-transcript.egs -c $SRC/v12/gc.egs -c $SRC/v12/variant.egs -c $SRC/v12/contig.egs -c $SRC/v12/ruler.egs -c $SRC/v12/framing.egs -c $SRC/v12/focus-transcript.egs -c $SRC/v12/focus-zoomed-transcript.egs -o $DST/render12.begs -L peregrine -O2 
+dauphin -c $SRC/v12/startup.egs -c $SRC/v12/lookup.egs -c $SRC/v12/jump.egs -o $DST/stick12.begs -L peregrine -O2
+dauphin -c $SRC/v12/boot.egs -o $DST/boot12.begs -L peregrine -O2
--- peregrine-data/src/core/version.rs
+++ peregrine-data/src/core/version.rs
@@ -1,7 +1,7 @@
 use std::collections::BTreeMap;
 use serde_cbor::Value as CborValue;
 
-const BE_VERSION: u32 = 11;
+const BE_VERSION: u32 = 12;
 
 #[derive(Clone)]
 pub struct VersionMetadata {
