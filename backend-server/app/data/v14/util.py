e22d92f6df4b33b3d38e2f724ed464dae3d5e72c
Dan Sheppard <dan@ebi.ac.uk>
2022-08-30 12:35:31 +0100

    Disable field-level compression now we have packet level.
    Remnants not yet actually removed from code: verified working first.

--- backend-server/app/data/v14/util.py
+++ backend-server/app/data/v14/util.py
@@ -13,8 +13,8 @@ def classified_numbers(result: dict, data: List[str], name: str):
 
     """
     (keys, values) = classify(data)
-    result[name + "_keys"] = compress("\0".join(keys))
-    result[name + "_values"] = compress(lesqlite2(values))
+    result[name + "_keys"] = "\0".join(keys)
+    result[name + "_values"] = lesqlite2(values)
 
 
 def starts_and_ends(result: dict, sizes: List[Tuple[int, int]], name: str):
@@ -32,8 +32,8 @@ def starts_and_ends(result: dict, sizes: List[Tuple[int, int]], name: str):
         name += "_"
     else:
         name = ""
-    result[name + 'starts'] = compress(lesqlite2(zigzag(delta([x[0] for x in sizes]))))
-    result[name + 'lengths'] = compress(lesqlite2(zigzag(delta([x[1] - x[0] for x in sizes]))))
+    result[name + 'starts'] = lesqlite2(zigzag(delta([x[0] for x in sizes])))
+    result[name + 'lengths'] = lesqlite2(zigzag(delta([x[1] - x[0] for x in sizes])))
 
 
 def starts_and_lengths(result: dict, sizes: List[Tuple[int, int]], name: str):
@@ -51,8 +51,8 @@ def starts_and_lengths(result: dict, sizes: List[Tuple[int, int]], name: str):
         name += "_"
     else:
         name = ""
-    result[name + 'starts'] = compress(lesqlite2(zigzag(delta([x[0] for x in sizes]))))
-    result[name + 'lengths'] = compress(lesqlite2(zigzag(delta([x[1] for x in sizes]))))
+    result[name + 'starts'] = lesqlite2(zigzag(delta([x[0] for x in sizes])))
+    result[name + 'lengths'] = lesqlite2(zigzag(delta([x[1] for x in sizes])))
 
 
 def domino_series_expand(data: List[Any], distance: int) -> List[Any]:
