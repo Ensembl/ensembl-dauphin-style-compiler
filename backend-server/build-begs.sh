f48eb73d1b31c868222304db0e044c589abe6cc4
Dan Sheppard <dan@ebi.ac.uk>
2022-12-08 10:51:17 +0000

    Jumping gene dots.

--- backend-server/build-begs.sh
+++ backend-server/build-begs.sh
@@ -7,5 +7,5 @@ export PATH="$PATH:$DIR/../dauphin/target/release"
 SRC="$DIR/egs-data/egs"
 DST="$DIR/egs-data/begs"
 
-dauphin -c $SRC/v15/focus-region.egs -c $SRC/v15/focus-gene.egs -c $SRC/v15/zoomed-seq.egs -c $SRC/v15/gene-overview.egs -c $SRC/v15/gene.egs -c $SRC/v15/transcript.egs -c $SRC/v15/zoomed-transcript.egs -c $SRC/v15/gc.egs -c $SRC/v15/variant.egs -c $SRC/v15/contig.egs -c $SRC/v15/ruler.egs -c $SRC/v15/framing.egs -c $SRC/v15/focus-transcript.egs -c $SRC/v15/focus-zoomed-transcript.egs -o $DST/render15.begs -L peregrine -O2 
+dauphin -c $SRC/v15/focus-gene-dots.egs -c $SRC/v15/focus-region.egs -c $SRC/v15/focus-gene.egs -c $SRC/v15/zoomed-seq.egs -c $SRC/v15/gene-overview.egs -c $SRC/v15/gene.egs -c $SRC/v15/transcript.egs -c $SRC/v15/zoomed-transcript.egs -c $SRC/v15/gc.egs -c $SRC/v15/variant.egs -c $SRC/v15/contig.egs -c $SRC/v15/ruler.egs -c $SRC/v15/framing.egs -c $SRC/v15/focus-transcript.egs -c $SRC/v15/focus-zoomed-transcript.egs -o $DST/render15.begs -L peregrine -O2 
 dauphin -c $SRC/v15/test.egs -c $SRC/v15/test-with-data.egs -o $DST/test15.begs -L peregrine -O2 
