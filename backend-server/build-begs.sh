05afd0cad650f4ab2e19ca70c3901a329ccfa763
Dan Sheppard <dan@ebi.ac.uk>
2023-01-18 01:03:23 +0000

    focus zoomed transcript to eard.

--- backend-server/build-begs.sh
+++ backend-server/build-begs.sh
@@ -8,7 +8,7 @@ export PATH="$PATH:$DIR/../../peregrine-eard/compiler/target/release"
 SRC="$DIR/egs-data/egs"
 DST="$DIR/egs-data/begs"
 
-dauphin -c $SRC/v16/focus-region.egs -c $SRC/v16/focus-zoomed-transcript.egs -o $DST/render16.begs -L peregrine -O2 
+dauphin -c $SRC/v16/focus-region.egs -o $DST/render16.begs -L peregrine -O2 
 dauphin -c $SRC/v16/test.egs -c $SRC/v16/test-with-data.egs -o $DST/test16.begs -L peregrine -O2 
 eard-compiler \
     -c $SRC/v16/framing.eard -c $SRC/v16/gc.eard -c $SRC/v16/contig.eard \
@@ -16,4 +16,5 @@ eard-compiler \
     -c $SRC/v16/gene-overview.eard -c $SRC/v16/gene.eard -c $SRC/v16/focus-gene.eard \
     -c $SRC/v16/focus-gene-dots.eard -c $SRC/v16/transcript.eard \
     -c $SRC/v16/focus-transcript.eard -c $SRC/v16/zoomed-transcript.eard \
+    -c $SRC/v16/focus-zoomed-transcript.eard \
     -o $DST/render16.eardo
