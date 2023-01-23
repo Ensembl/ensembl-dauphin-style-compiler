95d9fae8f14c1e982997aa6f5d200f151af95dbe
Dan Sheppard <dan@ebi.ac.uk>
2023-01-23 12:54:04 +0000

    Variant track initial commit.

--- backend-server/build-begs.sh
+++ backend-server/build-begs.sh
@@ -11,10 +11,11 @@ DST="$DIR/egs-data/begs"
 #dauphin -c $SRC/v16/test.egs -c $SRC/v16/test-with-data.egs -o $DST/test16.begs -L peregrine -O2 
 eard-compiler \
     -c $SRC/v16/other/framing.eard -c $SRC/v16/other/gc.eard -c $SRC/v16/other/contig.eard \
-    -c $SRC/v16/other/zoomed-seq.eard -c $SRC/v16/other/variant.eard -c $SRC/v16/other/ruler.eard \
-    -c $SRC/v16/gene/gene-overview.eard -c $SRC/v16/gene/gene.eard \
+    -c $SRC/v16/other/zoomed-seq.eard -c $SRC/v16/variant/variant-summary.eard \
+    -c $SRC/v16/other/ruler.eard -c $SRC/v16/gene/gene-overview.eard -c $SRC/v16/gene/gene.eard \
     -c $SRC/v16/gene/focus-gene.eard -c $SRC/v16/gene/focus-gene-dots.eard \
     -c $SRC/v16/gene/transcript.eard  -c $SRC/v16/other/focus-region.eard \
     -c $SRC/v16/gene/focus-transcript.eard -c $SRC/v16/gene/zoomed-transcript.eard \
     -c $SRC/v16/gene/focus-zoomed-transcript.eard -c $SRC/v16/other/focus-region.eard \
+    -c $SRC/v16/variant/focus-variant.eard \
     -o $DST/render16.eardo
