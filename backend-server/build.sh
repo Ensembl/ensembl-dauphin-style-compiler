f4a2b0137638e0ad657f5e60e60b4817c1ac45e7
Andres Veidenberg <andres@ebi.ac.uk>
2023-08-25 15:38:12 +0300

    Restyle variation (#84)

--- backend-server/build.sh
+++ backend-server/build.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 cd app
-export SOURCES_TOML=sources-dan-standalone.toml
+export SOURCES_TOML=sources-s3.toml
 uvicorn main:app --host 0.0.0.0 --port 3333 --workers 16
