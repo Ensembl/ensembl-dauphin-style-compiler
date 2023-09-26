5f76dcbfd3b8c63f5418c782401c7d3afc47fb09
Andres Veidenberg <andres@ebi.ac.uk>
2023-09-26 11:29:59 +0300

    Add trackAPI host

--- backend-server/app/model/expansions.py
+++ backend-server/app/model/expansions.py
@@ -1,6 +1,8 @@
 import logging
 import requests
+import toml
 from model.tracks import Track, Tracks
+from core.config import SOURCES_TOML
 
 class Expansions:
     def test(self, step):
@@ -15,18 +17,23 @@ class Expansions:
         tracks = Tracks()
         tracks.add_track("test",track)
         return tracks
+    
+    def _read_toml(self):
+        with open(SOURCES_TOML) as f:
+            toml_file = toml.loads(f.read())
+        return toml_file["apis"].get("track_api","localhost")
 
     def define_variation_track(self, track_id):
         # Read track metadata from Track API
-        print(track_id)
-        resp = requests.get(f"http://beta.ensembl.org/api/tracks/track/{track_id}", timeout=5)
+        track_api_host = self._read_toml()
+        resp = requests.get(f"{track_api_host}/track/{track_id}", timeout=5)
         if resp.status_code != requests.codes.ok:
             raise Exception(f"Track API request failed for track '{track_id}': {resp.reason}")
         track_data = resp.json()
         print(track_data)
         if("track_id" not in track_data or track_data["track_id"] != track_id):
             raise Exception(f"Track {track_id} not found in Track API payload: {track_data}")
-        #Hack until we rename datafiles with uuids. Example value: "dbsnp"
+        #Temporary hack until TrackAPI integrated for datafile location
         track_source = track_data["label"].split()[0].lower()
         track_data_ids = create_variant_track_data_ids(f"variant-{track_source}")
 
@@ -44,7 +51,7 @@ class Expansions:
         for track in [track_summary_view, track_details_view]:
             track.add_trigger(["track", "expand-variation", track_id]) # to turn a track on/off
             track.add_setting("name", ["track", "expand-variation", track_id, "name"]) # toggle track name on/off
-            #track.add_setting("display_order", ["track", "expand-variation", track_id, "display_order"]) # set track order
+            track.add_setting("display_order", ["track", "expand-variation", track_id, "display_order"]) # set track order
             track.add_value("track_id", track_id) # will be required for defining the track "leaf" in the tree of tracks
             track.add_value("track_name", track_data['label']) # inject track name into the track program
             track.add_value("display_order", track_data['display_order']) # initial track order
