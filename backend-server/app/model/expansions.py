35e92f9ae7e6b048d11e00980db404f49da06ff8
Andres Veidenberg <andres@ebi.ac.uk>
2023-09-28 18:46:49 +0300

    Use track API in expansion function (#91)
    * Use track API in expansion function

--- backend-server/app/model/expansions.py
+++ backend-server/app/model/expansions.py
@@ -1,6 +1,8 @@
 import logging
-
+import requests
+import toml
 from model.tracks import Track, Tracks
+from core.config import SOURCES_TOML
 
 class Expansions:
     def test(self, step):
@@ -15,10 +17,24 @@ class Expansions:
         tracks = Tracks()
         tracks.add_track("test",track)
         return tracks
+    
+    def _read_toml(self):
+        with open(SOURCES_TOML) as f:
+            toml_file = toml.loads(f.read())
+        return toml_file["apis"].get("track_api","localhost")
 
     def define_variation_track(self, track_id):
-        track_data = variation_tracks_map[track_id]
-        track_data_ids = create_variant_track_data_ids(track_id)
+        # Read track metadata from Track API
+        track_api_host = self._read_toml()
+        resp = requests.get(f"{track_api_host}/track/{track_id}", timeout=5)
+        if resp.status_code != requests.codes.ok:
+            raise Exception(f"Track API request failed for track '{track_id}': {resp.reason}")
+        track_data = resp.json()
+        if("track_id" not in track_data or track_data["track_id"] != track_id):
+            raise Exception(f"Track {track_id} not found in Track API payload: {track_data}")
+        #Temporary hack until TrackAPI integrated for datafile location
+        track_source = track_data["label"].split()[0].lower()
+        track_data_ids = create_variant_track_data_ids(f"variant-{track_source}")
 
         # While for the client, there is only one id for a track,
         # usually there are in fact several tracks associated with an id reported by the client;
@@ -34,10 +50,10 @@ class Expansions:
         for track in [track_summary_view, track_details_view]:
             track.add_trigger(["track", "expand-variation", track_id]) # to turn a track on/off
             track.add_setting("name", ["track", "expand-variation", track_id, "name"]) # toggle track name on/off
-            # track.add_setting("rank", ["track", "expand-variation", track_id, "rank"]) # setting to determine track order
+            #track.add_setting("rank", ["track", "expand-variation", track_id, "display_order"]) # set track order
             track.add_value("track_id", track_id) # will be required for defining the track "leaf" in the tree of tracks
-            track.add_value("track_name", track_data['track-name']) # inject track name into the track program
-            track.add_value("display_order", track_data['display-order']) # temporary, probably
+            track.add_value("track_name", track_data['label']) # inject track name into the track program
+            track.add_value("display_order", track_data['display_order']) # initial track order
 
         # define summary view settings
         track_summary_view.add_value("track_data_id", track_data_ids['summary-data-id'])
@@ -57,18 +73,6 @@ class Expansions:
 
         return tracks
 
-
-variation_tracks_map = {
-    'variant-dbsnp': {
-        'track-name': 'dbSNP short variants',
-        'display-order': '1001'
-    },
-    'variant-1000genomes': {
-        'track-name': '1000 Genomes short variants',
-        'display-order': '1000'
-    }
-}
-
 def create_variant_track_data_ids(track_id):
     """
     So far, the convention we are using for variant track ids
@@ -79,7 +83,7 @@ def create_variant_track_data_ids(track_id):
         the word "variant" and the rest of the id
     """
     id_parts = track_id.split('-')
-    id_parts = [id_parts[0], 'labels', *id_parts[1:]] # injject the word "labels" into the id
+    id_parts = [id_parts[0], 'labels', *id_parts[1:]] # inject the word "labels" into the id
     return {
         'zoomed-data-id': '-'.join(id_parts),
         'summary-data-id': track_id
