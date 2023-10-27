d1554fa617aa96e551db586262dc1d232b9c8820
Andres Veidenberg <andres@ebi.ac.uk>
2023-10-27 23:55:37 +0300

    Simplify

--- backend-server/app/model/datamodel.py
+++ backend-server/app/model/datamodel.py
@@ -32,12 +32,8 @@ class DataModel(object):
                 return self._species[species_name].chromosome(data_accessor, alias)
         return None
 
-    def species(self, alias):
-        species_name = self._species_aliases.get(alias)
-        if species_name is not None:
-            return self._species[species_name]
-        else:
-            return None        
+    def species(self, alias): # return Species() instance or None
+        return self._species.get(alias)
 
     def canonical_genome_id(self, alias):
         for (prefix, chr) in split_all(":", alias):
