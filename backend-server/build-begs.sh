a05f9ee82f920edce05eee3fd8f8808144ef629e
Andres Veidenberg <andres@ebi.ac.uk>
2024-02-01 15:58:21 +0200

    Update build script

--- backend-server/build-begs.sh
+++ backend-server/build-begs.sh
@@ -6,7 +6,7 @@ DST="./egs-data/begs"
 eard-compiler \
     -c $SRC/v16/other/framing.eard -c $SRC/v16/other/gc.eard -c $SRC/v16/other/contig.eard \
     -c $SRC/v16/other/zoomed-seq.eard \
-    -c $SRC/v16/variant/variant-summary.eard -c $SRC/v16/variant/variant-zoomed.eard \
+    -c $SRC/v16/variant/variant-summary.eard -c $SRC/v16/variant/variant-details.eard \
     -c $SRC/v16/other/ruler.eard -c $SRC/v16/gene/gene-overview.eard -c $SRC/v16/gene/gene.eard \
     -c $SRC/v16/gene/focus-gene.eard -c $SRC/v16/gene/focus-gene-dots.eard \
     -c $SRC/v16/gene/transcript.eard  -c $SRC/v16/other/focus-region.eard \
