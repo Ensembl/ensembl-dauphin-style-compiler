dfc889b0cfce1ae214f1383f131b26ee6ab7bf82
jyothishnt <joyjoy.nt@gmail.com>
2024-06-20 09:30:19 +0100

    fix highlighting outbound region bug

--- backend-server/app/data/v16/wiggle.py
+++ backend-server/app/data/v16/wiggle.py
@@ -21,16 +21,28 @@ def get_wiggle_data( data_accessor: DataAccessor, panel: Panel, data_file: str, 
         (data, start, end) = get_bigwig_stats(data_accessor, item, panel.start, panel.end)
     
     # clean & normalize input data range for eard (0..25)
+    overflow_flag = []
+    normalized_data = []
+    scores = []
+    x_x = []
     scale = 25/(data_range[1]-data_range[0])
     for i, x in enumerate(data):
         if x is None or math.isnan(x):
             x = 0
+            overflow_flag.append(0)
+            scores.append("{:.2f}".format(x))
         else:
+            scores.append("{:.2f}".format(x))
             x = round((x-data_range[0])*scale)
-            x = max(0, min(25, x))
-        data[i] = x
+            unbound_x = x
+            x_x.append(x)
+            x = max(0, min(25, x)) # 0-25
+            overflow_flag.append(1 if x != unbound_x else 0) # unbounds
+        normalized_data.append(x)
     return {
-        "values": data_algorithm("NDZRL", bytearray(data)),
+        "normalized_values": data_algorithm("NDZRL", bytearray(normalized_data)),
+        "conservation_scores": data_algorithm("SZ", scores),
+        "overflow_flag": data_algorithm("NDZRL", overflow_flag),
         "range": data_algorithm("NRL", [start, end])
     }
 
