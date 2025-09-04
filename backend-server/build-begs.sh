dec65ecd88835bace1bc354d0d7018e6e8723e32
Andres Veidenberg <andres@ebi.ac.uk>
2025-09-04 11:20:08 +0300

    Inital gene track

--- backend-server/build-begs.sh
+++ backend-server/build-begs.sh
@@ -1,20 +1,21 @@
 #! /bin/bash
 
-SRC="./egs-data/egs"
+SRC="./egs-data/egs/v16"
 DST="./egs-data/begs"
 
 eard-compiler \
-    -c $SRC/v16/other/framing.eard -c $SRC/v16/other/gc.eard -c $SRC/v16/other/contig.eard \
-    -c $SRC/v16/other/zoomed-seq.eard \
-    -c $SRC/v16/variant/variant-summary.eard -c $SRC/v16/variant/variant-details.eard \
-    -c $SRC/v16/other/ruler.eard -c $SRC/v16/gene/gene-overview.eard -c $SRC/v16/gene/gene.eard \
-    -c $SRC/v16/gene/focus-gene.eard -c $SRC/v16/gene/focus-gene-dots.eard \
-    -c $SRC/v16/gene/transcript.eard  -c $SRC/v16/other/focus-region.eard \
-    -c $SRC/v16/gene/focus-transcript.eard -c $SRC/v16/gene/zoomed-transcript.eard \
-    -c $SRC/v16/gene/focus-zoomed-transcript.eard -c $SRC/v16/other/focus-region.eard \
-    -c $SRC/v16/variant/focus-variant.eard -c $SRC/v16/variant/focus-variant-summary.eard \
-    -c $SRC/v16/variant/focus-variant-dots.eard -c $SRC/v16/regulation/regulation.eard \
-    -c $SRC/v16/compara/compara-summary.eard -c $SRC/v16/compara/compara-details.eard \
-    -c $SRC/v16/repeat/repeat-summary.eard -c $SRC/v16/repeat/repeat-details.eard \
-    -c $SRC/v16/simple-features/tssp.eard -c $SRC/v16/simple-features/cpg.eard -c $SRC/v16/simple-features/trna.eard \
+    -c $SRC/other/framing.eard -c $SRC/other/gc.eard -c $SRC/other/contig.eard \
+    -c $SRC/other/zoomed-seq.eard \
+    -c $SRC/variant/variant-summary.eard -c $SRC/variant/variant-details.eard \
+    -c $SRC/other/ruler.eard -c $SRC/gene/gene-overview.eard -c $SRC/gene/gene.eard \
+    -c $SRC/gene/focus-gene.eard -c $SRC/gene/focus-gene-dots.eard \
+    -c $SRC/gene/transcript.eard  -c $SRC/other/focus-region.eard \
+    -c $SRC/gene/focus-transcript.eard -c $SRC/gene/zoomed-transcript.eard \
+    -c $SRC/gene/focus-zoomed-transcript.eard -c $SRC/other/focus-region.eard \
+    -c $SRC/variant/focus-variant.eard -c $SRC/variant/focus-variant-summary.eard \
+    -c $SRC/variant/focus-variant-dots.eard -c $SRC/regulation/regulation.eard \
+    -c $SRC/compara/compara-summary.eard -c $SRC/compara/compara-details.eard \
+    -c $SRC/repeat/repeat-summary.eard -c $SRC/repeat/repeat-details.eard \
+    -c $SRC/simple-features/tssp.eard -c $SRC/simple-features/cpg.eard -c $SRC/simple-features/trna.eard \
+    -c $SRC/gene/sv-gene.eard \
     -o $DST/render16.eardo
