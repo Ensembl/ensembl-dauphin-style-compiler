f93eb2843e5a39bd57ba321f122350524b3f6294
Andres Veidenberg <andres@ebi.ac.uk>
2026-02-26 14:46:43 +0200

    Consolidate bigbed reading code

--- backend-server/app/data/v16/variant.py
+++ backend-server/app/data/v16/variant.py
@@ -1,7 +1,7 @@
 import logging
 from command.coremodel import DataHandler, Panel, DataAccessor
 from command.response import Response
-from model.bigbed import get_bigwig_stats, get_bigwig, get_bigbed
+from model.bigbed import get_bigwig_stats, get_bigwig, get_bigbed_fields
 from model.datalocator import AccessItem
 from data.v16.dataalgorithm import data_algorithm
 
@@ -68,44 +68,30 @@ class VariantSummaryDataHandler(DataHandler):
 
 
 def get_variant_labels(
-    data_accessor: DataAccessor, panel: Panel, filename: str, start: str | None=None, id: str | None=None
+    data_accessor: DataAccessor, panel: Panel, filename: str, start: str | None=None
 ) -> dict[str, bytearray]:
-    chrom = panel.get_chrom(data_accessor)
-    access_item = chrom.item_path(filename)
     try:
         if start: # only start is needed to fetch the variant
             panel.start = int(start)-1
             panel.end = panel.start+2
-        data = get_bigbed(data_accessor, access_item, panel.start, panel.end)
-        starts = []
-        lengths = []
-        ids = []
-        varieties = []
-        groups = []
-        consequence = []
-        chromosomes = []
-        alleles = []
-        for start, end, rest in data:
-            rest = rest.split()
-            chromosomes.append(chrom.name)
-            starts.append(start)
-            lengths.append(end - start)
-            ids.append(rest[0])
-            varieties.append(rest[1])
-            alleles.append(allele_sequence(rest[2], rest[3]))
-            groups.append(int(rest[4]))
-            consequence.append(rest[5])
+        fields = get_bigbed_fields(
+            data_accessor, panel, filename,
+            ["id", "variety", "ref", "alt", "group", "consequence"]
+        )
+        lengths = [end - start for start, end in zip(fields["start"], fields["end"])]
+        alleles = [allele_sequence(ref, alt) for ref, alt in zip(fields["ref"], fields["alt"])]
+        groups = [int(group) for group in fields["group"]]
     except Exception as e:
         logging.error(e)
     return {
-        "chromosome": data_algorithm("SZ", chromosomes),
-        "start": data_algorithm("NDZRL", starts),
+        "chromosome": data_algorithm("SZ", fields["chr"]),
+        "start": data_algorithm("NDZRL", fields["start"]),
         "length": data_algorithm("NDZRL", lengths),
-        "id": data_algorithm("SZ", ids),
-        "variety": data_algorithm("SYRLZ", varieties),
+        "id": data_algorithm("SZ", fields["id"]),
+        "variety": data_algorithm("SYRLZ", fields["variety"]),
         "alleles": data_algorithm("SYRLZ", alleles),
         "group": data_algorithm("NRL", groups),
-        "consequence": data_algorithm("SYRLZ", consequence),
+        "consequence": data_algorithm("SYRLZ", fields["consequence"]),
     }
 
 
@@ -121,5 +107,5 @@ class VariantLabelsDataHandler(DataHandler):
         self, data_accessor: DataAccessor, panel: Panel, scope: dict, accept: str
     ) -> dict:
         return get_variant_labels(
-            data_accessor, panel, self.get_datafile(scope), self.get_scope(scope,"start"), self.get_scope(scope,"id")
+            data_accessor, panel, self.get_datafile(scope), self.get_scope(scope,"start")
         )
