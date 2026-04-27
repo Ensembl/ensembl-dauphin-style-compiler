f534c808c198209f3db36a3b06c750e81be9ac45
Andres Veidenberg <andres@ebi.ac.uk>
2026-04-27 11:31:03 +0300

    Rename focus transcript tracks

--- backend-server/build-begs.sh
+++ backend-server/build-begs.sh
@@ -9,14 +9,14 @@ eard-compiler \
     -c $SRC/variant/variant-summary.eard -c $SRC/variant/variant-details.eard \
     -c $SRC/other/ruler.eard -c $SRC/gene/gene-overview.eard -c $SRC/gene/gene.eard \
     -c $SRC/gene/focus-gene.eard -c $SRC/gene/focus-gene-dots.eard \
-    -c $SRC/gene/transcript.eard  -c $SRC/other/focus-region.eard \
-    -c $SRC/gene/focus-transcript.eard -c $SRC/gene/focus-single-transcript.eard -c $SRC/gene/zoomed-transcript.eard \
-    -c $SRC/gene/focus-zoomed-transcript.eard -c $SRC/other/focus-region.eard \
+    -c $SRC/gene/transcript.eard  -c $SRC/gene/focus-transcript.eard -c $SRC/other/focus-region.eard \
+    -c $SRC/gene/focus-gene-transcripts.eard -c $SRC/gene/zoomed-transcript.eard \
+    -c $SRC/gene/focus-gene-zoomed-transcripts.eard -c $SRC/other/focus-region.eard \
+    -c $SRC/gene/sv-gene.eard -c $SRC/gene/sv-gene-overview.eard -c $SRC/gene/sv-transcript.eard \
     -c $SRC/variant/focus-variant.eard -c $SRC/variant/focus-variant-summary.eard \
     -c $SRC/variant/focus-variant-dots.eard -c $SRC/regulation/regulation.eard \
     -c $SRC/variant/sv-details.eard -c $SRC/variant/sv-summary.eard \
     -c $SRC/compara/compara-summary.eard -c $SRC/compara/compara-details.eard \
     -c $SRC/repeat/repeat-summary.eard -c $SRC/repeat/repeat-details.eard \
     -c $SRC/simple-features/tssp.eard -c $SRC/simple-features/cpg.eard -c $SRC/simple-features/trna.eard \
-    -c $SRC/gene/sv-gene.eard -c $SRC/gene/sv-gene-overview.eard -c $SRC/gene/sv-transcript.eard \
     -o $DST/render16.eardo
